139b55fbd8cba0380b634c854d74f6d41e81acb8
joshua <carpenterskeys@gmail.com>
2022-05-05 13:16:13 -0400

    centering/alignmnt on navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Layout.module.scss';
+import { AiFillGithub } from 'react-icons/ai';
 
 export default function Layout({ pageTitle }) {
 
@@ -13,16 +14,18 @@ export default function Layout({ pageTitle }) {
                 <div id={styles.mainUl}>
                     <ul>
                         <li><h1><Link href='/'>Joshua</Link></h1></li>
-                        <li><Link href='/projects'>projects</Link></li>.
-                        <li><Link href='/resume'>resume</Link></li>.
-                        <li><Link href='/about'>about</Link></li>.
-                        <li><Link href='/contact'>contact</Link></li>()
+                            <li><Link href='/projects'>projects</Link></li><div>.</div>
+                            <li><Link href='/resume'>resume</Link></li><div>.</div>
+                            <li><Link href='/about'>about</Link></li><div>.</div>
+                            <li><Link href='/contact'>contact</Link></li><div>()</div>
                     </ul>
                 </div>
-                <div className='divider' id={styles.bar}><div style={{height: '50%', backgroundColor: 'var(--prim-clr)', width: '3px'}}></div> </div>
-                <ul id={styles.offsiteUl}>
-                    <li><a className={styles.link} rel="noreferrer noopener" target="_blank" href="https://github.com/CarpentersKeys">Github</a></li>
-                </ul>
+                <div className='divider' id={styles.bar}><div style={{ height: '50%', backgroundColor: 'var(--prim-clr)', width: '3px' }}></div> </div>
+                <div id={styles.offsiteUl}>
+                    <ul>
+                        <li><a className={styles.iconLink} rel="noreferrer noopener" target="_blank" href="https://github.com/CarpentersKeys"><div id={styles.icon}><AiFillGithub /></div></a></li>
+                    </ul>
+                </div>
             </nav>
         </div>
     )

550d390bc4acdf6ebe3f7bdf994bd72189b7e59a
joshua <carpenterskeys@gmail.com>
2022-05-05 21:19:34 -0400

    style tweaking, accessibility, icons

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,32 +1,39 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import styles from '../styles/Layout.module.scss';
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillGithub, } from 'react-icons/ai';
+import { WiSunset } from 'react-icons/wi';
 
 export default function Layout({ pageTitle }) {
 
     return (
         <div id={styles.layout}>
             <Head>
-                <title>{`Joshua O&apos;Neill Web Developer Portfolio: ${pageTitle}`}</title>
+                <title>{`Joshua O'Neill Web Developer Portfolio. Page: ${pageTitle}`}</title>
             </Head>
             <nav>
                 <div id={styles.mainUl}>
                     <ul>
-                        <li><h1><Link href='/'>Joshua</Link></h1></li>
-                            <li><Link href='/projects'>projects</Link></li><div>.</div>
-                            <li><Link href='/resume'>resume</Link></li><div>.</div>
-                            <li><Link href='/about'>about</Link></li><div>.</div>
-                            <li><Link href='/contact'>contact</Link></li><div>()</div>
+                        <li><h1><WiSunset id={styles.icon}/><Link href='/'>Joshua</Link></h1></li>
+                        <li><Link href='/projects'>projects</Link></li><div>.</div>
+                        <li><Link href='/resume'>resume</Link></li><div>.</div>
+                        <li><Link href='/about'>about</Link></li><div>.</div>
+                        <li><Link href='/contact'>contact</Link></li><div>()</div>
                     </ul>
                 </div>
                 <div className='divider' id={styles.bar}><div style={{ height: '50%', backgroundColor: 'var(--prim-clr)', width: '3px' }}></div> </div>
                 <div id={styles.offsiteUl}>
                     <ul>
-                        <li><a className={styles.iconLink} rel="noreferrer noopener" target="_blank" href="https://github.com/CarpentersKeys"><div id={styles.icon}><AiFillGithub /></div></a></li>
+                        <li>
+                            <a aria-label='Joshua&apos;s git hub' className={styles.iconLink} rel="noreferrer noopener" target="_blank" href="https://github.com/CarpentersKeys">
+                                <div >
+                                    <AiFillGithub  />
+                                </div>
+                            </a>
+                        </li>
                     </ul>
                 </div>
-            </nav>
-        </div>
+            </nav >
+        </div >
     )
 }

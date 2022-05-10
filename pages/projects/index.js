e0df6b909f95c1fe10bea9c10eebcb38bf1e774f
joshua <carpenterskeys@gmail.com>
2022-05-09 22:07:25 -0400

    grapql

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,52 +1,91 @@
 import Image from 'next/image';
 import styles from '../../styles/Projects.module.scss'
+// import { JSDOM } from 'jsdom';
 
 export async function getStaticProps() {
 
-    return fetch('https://api.github.com/users/CarpentersKeys/repos')
-        .then(resp => resp.json())
-        .then(async json => (
-            Promise.all(
-                json.map(repo => fetch(repo.statuses_url))
-            )
-                .then(respArr => (Promise.all(respArr.map((resp) => resp.json()))))
-                .then(statuses => (
-                    {
-                        props: {
-                            repos: json,
-                            statuses,
-                        }
-                    })
-                )
+    // fetch logic
+
+    // get repos from github
+    fetch('https://api.github.com/users/CarpentersKeys/repos')
+        // check to make sure response ok and json
+        .then(resp => {
+            return resp.ok && resp.headers.get('Content-Type').includes('application/json') && resp.json()
+                || new Error(`github fetch error status: \n${resp.status}\n${resp.headers.get('Content-Type')}`)
+        })
+        .catch(err => err)
+        // filter repos without 'portfolio' topic
+        // map over repos
+        // make fetch for languages, and html preview image
+        // format into rep
+        // format all repos into {props: ...}
+        .then(repos => repos.filter(e => e.topics.includes('portfolio')))
+        .then(repos => (
+            repos.map(async (repo, i) => {
+                // const languages = await fetch(repo.languages_url)
+                // JSDOM.fromUrl(repo.html_url.contentType)
+                    // .then(result => console.log(result))
+                    console.log(repos[0]);
+
+            })
         ))
+    // .then(async json => {
+    //     return Promise.all(
+    //         json.map(repo => {
+    //             return {
+    //                 languages: fetch(repo.languages_url),
+    //                 previewImg: fetch(repo.html_url)
+    //             }
+
+    //         })
+    //     )
+    //         .then(respArr => (Promise.all(respArr.map((resp) => resp.json()))))
+    //         .then(langs => {
+    //             const repos = json
+    //                 .filter(e => e.topics.includes('portfolio'))
+    //                 .map((e, i) => {
+    //                     return {
+    //                         ...e,
+    //                         languages: langs[i]
+    //                     }
+    //                 })
+    //             return {
+    //                 props: {
+    //                     repos
+    //                 }
+    //             };
+    //         })
+    // })
+    return {props: 'aerg'};
 }
 
 
-export default function Projects({ repos, statuses }) {
-    console.log('repos:', repos);
-    console.log('statuses:', statuses)
+export default function Projects({ repos }) {
 
     return (
         <div className={styles.background}>
-            <section className={styles.projects}>
+            {/* <section className={styles.projects}>
                 <h2>projects</h2>
 
                 {
                     repos.map((repo, ind) => (
                         <article id={styles.firstProj} key={ind}>
-                            <h4>porjstatus</h4>
+                            <h4>{
+                                repo.homepage !== '' && <a target='_blank' rel='noopener noreferrer' href={repo.homepage}>deployed</a>
+                                || <a target='_blank' rel='noopener noreferrer' href={repo.html_url}>in progress</a>
+                            }</h4>
                             <h3>{repo.name}</h3>
                             <p>{repo.description}</p>
                             <div id={styles.techCont}>
-                                <h4>tech</h4>
                                 <div id={styles.techUl}>
                                     <ul>
-                                        horizontal list
-                                        <li>html</li>
-                                        <li>css</li>
-                                        <li>js</li>
-                                        <li>next</li>
-                                        <li>react</li>
+                                        {
+                                            Object.keys(repo.languages).map((e, i) => <li key={e + i}>{e}</li>)
+                                                .concat(
+                                                    repo.topics.filter(e => e !== 'portfolio')
+                                                        .map((e, i) => <li key={e + i}>{e}</li>)
+                                                ).slice(0, 5)
+                                        }
                                     </ul>
                                 </div>
                             </div>
@@ -57,7 +96,7 @@ export default function Projects({ repos, statuses }) {
                     ))
                 }
 
-            </section>
+            </section> */}
         </div>
     )
 }

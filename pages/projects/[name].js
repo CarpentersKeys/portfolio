9312dd5806fa8e41288146beded1ea31e0c1dac6
joshua <carpenterskeys@gmail.com>
2022-05-31 10:22:30 -0400

    semantic jsx for dynamically generated Project pages

--- pages/projects/[name].js
+++ pages/projects/[name].js
@@ -1,24 +1,63 @@
 import graphqlFetch from "../../lib/graphqlFetch";
 import { githubReposQuery as query } from "../../lib/queryStrings";
 import styles from '../../styles/Project.module.scss';
+import Image from 'next/image';
 
 export default function Project(props, ...rest) {
     const languages = props.languages.nodes.map(lang => lang.name)
     const topics = props.repositoryTopics.nodes.map(obj => obj.topic.name)
     const {
         name,
+        homepageUrl,
+        url: githubUrl,
         openGraphImageUrl: imageUrl,
         description,
-        homepageUrl,
         pushedAt: lastPushAt,
-        url: githubUrl,
     } = props;
 
-    console.log(rest)
     return (
         <div>
-            <h1 id={styles.title}>{name}</h1>
-        </div>
+            <section className={styles.project}>
+                <span>
+                    <h2 id={styles.title}>{name}</h2>
+                    {
+                        homepageUrl &&
+                        <span className={styles.divider}>
+                            <a
+                                target='_blank' rel='noreferrer'
+                                href={homepageUrl}>
+                                Deployment
+                            </a>
+                            |
+                        </span>
+                    }
+                    <a
+                        target='_blank' rel='noreferrer'
+                        href={homepageUrl || githubUrl}>
+                        Github
+                    </a>
+                </span>
+                <Image
+                    alt='repository image'
+                    className={styles.image}
+                    // maybe format images to be wide bannerlike things with a couple items on them
+                    src={imageUrl} width='300px' height='300px'
+                />
+                <h4>Last push:</h4>
+                <span>{lastPushAt.match(/[\d]{4}-[\d]{2}-[\d]{2}/)}</span>
+                <p>{description}</p>
+                <ul className={styles.tagsCont}>
+                    {
+                        languages.concat(topics)
+                            .map((tag, ind) => (
+                                <li className={styles.tag} key={ind}>
+                                    {tag}
+                                </li>
+                            ))
+                    }
+                </ul>
+            </section>
+        </div >
     )
 }
 
